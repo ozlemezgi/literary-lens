b07b2592118f06f8dbc020de4c28fdef711d4385
ozlemezgi <ozlemezgisari@icloud.com>
2023-04-13 22:30:15 +0300

    worked with google book API

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,34 @@
 import React from 'react'
 
-function Card() {
+function Card({book}) {
+    console.log(book);
   return (
     <>
-    <div className="card">
-        <img src="./images/content.jpeg" alt="" />
-        <div className="bottom">
-            <h3 className="title">Book Name</h3>
-            <hr></hr>
-            <p className='author'>Author</p>
-
-        </div>
-    </div>
+    {
+        // Map over the book data passed as props and create a Card component for each book
+        book.map((item)=>{
+          // Check if book has a thumbnail image and author information
+          let thumbnail =
+            item.volumeInfo.imageLinks &&
+            item.volumeInfo.imageLinks.smallThumbnail;
+          let author = item.volumeInfo.authors;
+          // If the book has a thumbnail image and author information, render a Card component
+          if (thumbnail != undefined && author != undefined) {
+            return (
+              <>
+                <div className="card">
+                  <img src={thumbnail} alt="" />
+                  <div className="bottom">
+                    <h3 className="title">{item.volumeInfo.title}</h3>
+                    <hr></hr>
+                    <p className="author">{author}</p>
+                  </div>
+                </div>
+              </>
+            );
+          }
+        })
+    }
     </>
   )
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card';
+import axios from 'axios';
 
 function Main() {
+  // Set up state for search input and book data
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
+
+  // Function to search for books using Google Books API
+  const searchBook = (evt) => {
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q='+ezgi+'&maxResults=40&key=AIzaSyCPwNMiuqw7q5m4d5edr-x8e8f28V6-Rn4"
+      )
+      // Set the book data state to the response data
+      .then((res) => setData(res.data.items))
+      .catch((err) => console.log(err));
+  };
   return (
     <>
+      {/* Header section start*/}
       <div className="header">
         <div className="row1">
           <img src="./images/kitaplar.jpg" alt="" />
@@ -13,26 +29,24 @@ function Main() {
         </div>
         <div className="row2">
           <h2>Literary Lens</h2>
+          {/* Search bar start */}
           <div className="search">
-            <input type="text" placeholder="Enter your book name" />
-            <button>
-              <i class="fa-sharp fa-magnifying-glass"></i>
+            //TODO:Performing a search when the enter key is pressed.
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              type="text"
+              placeholder="Enter your book name"
+            />
+            {/* Button to trigger search function */}
+            <button onClick={searchBook}>
+              <i className="fa-sharp fa-magnifying-glass"></i>
             </button>
           </div>
         </div>
       </div>
-      <div className="container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      {/* Container for book cards start */}
+      <div className="container">{<Card book={bookData} />}</div>
     </>
   );
 }
